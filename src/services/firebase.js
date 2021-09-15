3556b09561e1f3612aa0459cf051012566666728
KuldeepSingh <kul8003995609deep@gmail.com>
2021-09-15 12:01:07 +0530

    merging confict

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,43 +3,27 @@ import { getAuth ,GoogleAuthProvider, signInWithPopup, setPersistence,browserLoc
 import firebaseConfig from '../config/firebase-config';
 
 const app = initializeApp(firebaseConfig);
-<<<<<<< HEAD
-const auth = getAuth(app); //Authantication variable contain Authantication related details
-=======
 const auth = getAuth(); //Authantication variable contain Authantication related details
->>>>>>> 6251541c8ba6d1309774bdefefb71800b9853216
 const googleProvider = new GoogleAuthProvider();//provider for google signin
 
 
 //Function for signin
 const signInGoogle = async ()=>{
-<<<<<<< HEAD
-   
-=======
     try{
->>>>>>> 6251541c8ba6d1309774bdefefb71800b9853216
     setPersistence(auth, browserLocalPersistence);
     googleProvider.setCustomParameters({
         'hd': 'poornima.edu.in'
     });
     await signInWithPopup(auth,googleProvider).catch((error)=> alert(error.message));
-<<<<<<< HEAD
-=======
     }catch(error)
     {
         const mes= error.message;
     }
->>>>>>> 6251541c8ba6d1309774bdefefb71800b9853216
 };
 
 //Functior used To sign Out
 const signOutGoogle = async ()=>{
     await auth.signOut().catch((error)=>alert(error));
 }
-<<<<<<< HEAD
-
 export { auth,signInGoogle,signOutGoogle};
-=======
-export { auth,signInGoogle,signOutGoogle};
-export default app;
->>>>>>> 6251541c8ba6d1309774bdefefb71800b9853216
+//export default app;
