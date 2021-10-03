e93a5b001ddb473296a565b1842aab7803422a7d
KuldeepSingh <kul8003995609deep@gmail.com>
2021-10-03 20:43:58 +0530

    getting teacher data from the firestore
    added the function for getting all the current teacher details according to the department

--- src/components/Pages/Registration/RegisterationForm.jsx
+++ src/components/Pages/Registration/RegisterationForm.jsx
@@ -1,10 +1,10 @@
-import { doc, getDoc} from '@firebase/firestore';
+import { doc, getDoc } from '@firebase/firestore';
 import { React, useState } from 'react';
 import { db, auth, insertFirebaseDocument } from '../../../services/firebase';
 import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
 import { Avatar, Grid, TextField, Typography, Box, Autocomplete, Button } from '@mui/material';
-import { AvatarIcon} from '../../Import';
+import { AvatarIcon } from '../../Import';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DatePicker from '@mui/lab/DatePicker';
@@ -23,35 +23,40 @@ export const CheckRegistration = async (user) => {
 
     if (register === 'tch') {
         const ref = doc(db, "staff", docId);
-        await getDoc(ref).then((docSnap) => { if (docSnap.exists()) { register = 'staff' } else { register = 'staff' } }).catch((error) => alert(error))
-
+        await getDoc(ref).then((docSnap) => {
+            if (docSnap.exists()) { register = 'staff' }
+            else { register = 'staff' }
+        }).catch((error) => alert(error))
     } else {
         const ref = doc(db, "students", docId);
-        await getDoc(ref).then((docSnap) => { if (docSnap.exists()) { register = 'std'; userData=docSnap.data(); } else { register = 'stdn'; userData=null;} }).catch((error) => alert(error))
+        await getDoc(ref).then((docSnap) => {
+            if (docSnap.exists()) { register = 'std'; userData = docSnap.data(); }
+            else { register = 'stdn'; userData = null; }
+        }).catch((error) => alert(error))
     }
     // console.log(register);
     return register;
 }
 
 //for checking the department courses and setting them and then return arrary [dep, course, spec]
-function getCoursesAndSpecilization(email){
+function getCoursesAndSpecilization(email) {
     let course;
     let specilization;
     let dep;
 
     //checking for course from email
-    if(email.indexOf('bca')!==-1){
-        dep='sce';
-        course='bca';
-        specilization='gen';
-        if(email.indexOf('bcaai')!==-1){
-            specilization='ai';
-        }else if(email.indexOf('bcaitms')!==-1){
-            specilization='itms';
-        }else if(email.indexOf('bcamais')!==-1){
-            specilization='mais';
-        }else if(email.indexOf('bcads')!==-1){
-            specilization="ds";
+    if (email.indexOf('bca') !== -1) {
+        dep = 'sce';
+        course = 'bca';
+        specilization = 'gen';
+        if (email.indexOf('bcaai') !== -1) {
+            specilization = 'ai';
+        } else if (email.indexOf('bcaitms') !== -1) {
+            specilization = 'itms';
+        } else if (email.indexOf('bcamais') !== -1) {
+            specilization = 'mais';
+        } else if (email.indexOf('bcads') !== -1) {
+            specilization = "ds";
         }
     }
     return [dep, course, specilization]
@@ -74,10 +79,10 @@ export default function RegisterationForm({ submitCallback }) {
         console.log(form.regno);
 
         let courseDetails = getCoursesAndSpecilization(docId);
-        const data= { 
+        const data = {
             name: auth.currentUser.displayName,
             regno: form.regno.value.toUpperCase(),
-            dob:form.dob.value,
+            dob: form.dob.value,
             email: auth.currentUser.email,
             mobile: parseInt(form.personalMobile.value),
             address: form.address.value,
@@ -88,12 +93,12 @@ export default function RegisterationForm({ submitCallback }) {
             fatherName: form.fatherName.value,
             fatherMobile: parseInt(form.fatherMobile.value),
             motherName: form.motherName.value,
-            motherMobile: form.motherMobile.value==null? parseInt('0'): parseInt(form.motherMobile.value),
-            acadmics:{ 
-                marks10th: parseInt(form.marks10.value), 
+            motherMobile: form.motherMobile.value == null ? parseInt('0') : parseInt(form.motherMobile.value),
+            acadmics: {
+                marks10th: parseInt(form.marks10.value),
                 marks10thunit: form.marks10unit.value,
-                marks12th: parseInt(form.marks12.value), 
-                marks12thunit: form.marks12unit.value 
+                marks12th: parseInt(form.marks12.value),
+                marks12thunit: form.marks12unit.value
             }
         }
         //submiting the form
@@ -103,11 +108,11 @@ export default function RegisterationForm({ submitCallback }) {
 
     return (
         <div>
-            <Container component="main" maxWidth="md" sx={{textAlign:'center'}}>
-            <Typography sx={{ padding:'20px'}} variant="h5"  gutterBottom>Registration Form</Typography>
+            <Container component="main" maxWidth="md" sx={{ textAlign: 'center' }}>
+                <Typography sx={{ padding: '20px' }} variant="h5" gutterBottom>Registration Form</Typography>
                 <Paper sx={{ padding: "20px 20px 20px 20px" }}>
                     <form id="reg-form" method="POST" onSubmit={(e) => SubmitForm(e)}>
-                        <Typography variant="h6"  gutterBottom>Personal Details</Typography>
+                        <Typography variant="h6" gutterBottom>Personal Details</Typography>
                         <Box style={{ margin: "auto" }}>
                             <Avatar alt="Avatar Icon" sx={{ width: 150, height: 150, margin: "auto" }} src={AvatarIcon} />
                         </Box>
@@ -116,7 +121,7 @@ export default function RegisterationForm({ submitCallback }) {
                                 <TextField label="Registation no" name="regno" fullWidth required />
                             </Grid>
                             <Grid xs={12} item>
-                                <TextField sx={{textAlign:'left'}} type="email" label="Personal Email" name="personalEmail" fullWidth required />
+                                <TextField sx={{ textAlign: 'left' }} type="email" label="Personal Email" name="personalEmail" fullWidth required />
                             </Grid>
                             <Grid xs={12} item>
                                 <TextField type="tel" label="Personal Mobile" name="personalMobile" fullWidth required />
@@ -141,7 +146,7 @@ export default function RegisterationForm({ submitCallback }) {
                                 />
                             </Grid>
                             <Grid xs={12} sm={6} item>
-                                <TextField label="Father Name" name="fatherName"fullWidth required />
+                                <TextField label="Father Name" name="fatherName" fullWidth required />
                             </Grid>
                             <Grid xs={12} sm={6} item>
                                 <TextField
@@ -152,7 +157,7 @@ export default function RegisterationForm({ submitCallback }) {
                                     required />
                             </Grid>
                             <Grid xs={12} sm={6} item>
-                                <TextField label="Mother Name" name="motherName" fullWidth required/>
+                                <TextField label="Mother Name" name="motherName" fullWidth required />
                             </Grid>
                             <Grid xs={12} sm={6} item>
                                 <TextField label="Mother Phone Number" name="motherMobile" type="number" fullWidth />
@@ -161,35 +166,35 @@ export default function RegisterationForm({ submitCallback }) {
                                 <TextField label="Address" name="address" fullWidth />
                             </Grid>
                         </Grid>
-                        <Typography sx={{mt:2}} variant="h6" gutterBottom>Acadmic Details</Typography>
+                        <Typography sx={{ mt: 2 }} variant="h6" gutterBottom>Acadmic Details</Typography>
                         <Grid sx={{ mt: 1 }} container spacing={3}>
                             {/* 10ths Marks */}
-                        <Grid xs={12} sm={3} item>
+                            <Grid xs={12} sm={3} item>
                                 <TextField label="10th Marks" name="marks10" type="" fullWidth required />
-                        </Grid>
-                        {/* 10th Marks Unit */}
-                        <Grid item xs={12} sm={3}>
-                        <Autocomplete
+                            </Grid>
+                            {/* 10th Marks Unit */}
+                            <Grid item xs={12} sm={3}>
+                                <Autocomplete
 
                                     disablePortal
-                                    options={['%','CGPA']}
+                                    options={['%', 'CGPA']}
                                     renderInput={(params) => <TextField {...params} name="marks10unit" label="Unit" required />}
                                 />
-                        </Grid>
-                        {/* 12th Marks Unit */}
-                        <Grid xs={12} sm={3} item>
+                            </Grid>
+                            {/* 12th Marks Unit */}
+                            <Grid xs={12} sm={3} item>
                                 <TextField label="12th Marks" type="number" name="marks12" fullWidth required />
-                        </Grid>
-                        {}
-                        <Grid item xs={12} sm={3}>
-                        <Autocomplete
+                            </Grid>
+                            { }
+                            <Grid item xs={12} sm={3}>
+                                <Autocomplete
                                     disablePortal
-                                    options={['%','CGPA']}
+                                    options={['%', 'CGPA']}
                                     renderInput={(params) => <TextField {...params} name="marks12unit" label="Unit" required />}
                                 />
+                            </Grid>
                         </Grid>
-                        </Grid>
-                        <Button type="submit" variant="contained" sx={{mt:5}}> Register</Button>
+                        <Button type="submit" variant="contained" sx={{ mt: 5 }}> Register</Button>
                     </form>
                 </Paper>
             </Container>
