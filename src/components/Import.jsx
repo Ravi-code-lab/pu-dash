8e1185010a541cc5e9e0bd05fe0f9f38ad51efe8
Ravi Raj Yadav <ravirajyadav8432@gmail.com>
2021-10-14 12:12:22 +0530

    Login Testing

--- src/components/Import.jsx
+++ src/components/Import.jsx
@@ -3,6 +3,7 @@ import Pulogo from '../assets/logo/PUicon.svg';
 import puicon from '../assets/logo/PUicon.svg';
 import puiconname from '../assets/logo/pu-logo.png';
 import pulogo from '../assets/logo/pulogo.png';
+import avlogo from '../assets/images/logo.png';
 
 
 
@@ -10,5 +11,5 @@ import AvatarIcon from '../assets/icon/AvatarIcon.svg'
 // Poornima University
 import backgr from '../assets/images/background.jpg'
 //
-export {AvatarIcon,pulogo,puicon,Pulogo,puiconname,backgr,Timer};
+export {AvatarIcon,pulogo,puicon,Pulogo,avlogo,puiconname,backgr,Timer};
 
--- src/components/Pages/Login/Login.jsx
+++ src/components/Pages/Login/Login.jsx
@@ -3,31 +3,43 @@ import { Button ,Container,Box ,Paper} from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import { React } from 'react'
 import { signInGoogle } from '../../../services/firebase';
-import {puicon,puiconname} from '../../Import';
-
-
+import {puicon,puiconname,avlogo} from '../../Import';
 const useStyle = makeStyles((theme) => ({
 
   Container:{
     height:'100vh',
     display:"flex",
-    
   },
-  
   Box:{
     textAlign: 'center',
      margin:"auto",
      height:"100%",
      width:"100%", 
-   
   },
   BoxLeft:{
+    textAlign:"-webkit-center",
      height:"100%",
      width:"100%" 
   },
   BoxRight:{
-     height:"100%",
-     width:"100%" 
+    textAlign:"-webkit-center",
+    height:"100%",
+    width:"100%",
+    backgroundImage:"url(https://firebasestorage.googleapis.com/v0/b/poornima-dashboord.appspot.com/o/bg.jpg?alt=media&token=03bf512a-e176-420a-9968-8ec68c170ccf)",
+    
+    borderRadius:"0 20px 20px 0",
+    backgroundSize:"100%",
+    '@media only screen and (max-width: 600px)': {
+      borderRadius:"0 0 20px 20px"
+    },
+    
+  },
+  BoxRightMain:{
+    backgroundColor:"#007FFF",
+    borderRadius:"0 20px 20px 0",
+    '@media only screen and (max-width: 600px)': {
+      borderRadius:"0 0 20px 20px"
+    },
   },
   Papper:{
     borderRadius:"20px",
@@ -35,7 +47,7 @@ const useStyle = makeStyles((theme) => ({
     display:"flex",
     alignItems:"center",
     '@media only screen and (max-width: 600px)': {
-      display:""
+      display:"block"
     },
   }
 
@@ -57,20 +69,24 @@ export default function Login() {
               <img src={puicon} alt="logo"></img>
               <img src={puiconname} alt="logo"></img>
             </div>
-            <Button variant="contained" color="primary"sx={{margin: 2,fontSize: 20}} onClick={() => signInGoogle()}>
+            <Button variant="contained" color="primary"sx={{margin: 2}} onClick={() => signInGoogle()}>
                 Login
             </Button>
          </Box>
          <Box className={classes.BoxRight}>
-          <Box>
-            hgtddtd
-          </Box>
+           <Box className={classes.BoxRightMain} >
+            <div className=" a6">
+              <img src={avlogo} alt="logo"></img>
+              <img src={puiconname} alt="logo"></img>
+            </div>
+            <Button variant="contained" color="primary"sx={{margin: 2}} onClick={() => signInGoogle()}>
+                Login
+            </Button>
+            </Box>
          </Box>
-          
         </Paper>
       </Box>
       <Box >
-
       </Box>
     </Container>
     </>
